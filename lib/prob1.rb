c21a35d7aa091550c839064e7332cd8f807a4f91
Sean <sjgallen@gmail.com>
2016-07-10 21:26:20 -0500

    removed unneeded files

--- lib/prob1.rb
+++
@@ -1,6 +0,0 @@
-y = *1...1000
-
-
-z = y.find_all {|e| e % 3 == 0 || e % 5 == 0 }.reduce(:+)
-
-puts z
--- lib/prob7.rb
+++
@@ -1,9 +0,0 @@
-require 'prime'
-
-class PrimeNumbers
-  def prime_index_value(index)
-    Prime.first(index).pop
-  end
-end
-
-
